4bc5d46c7d2e268c1e240c5714a95a5e17640e57
rockz <rockzhangenyan@gmail.com>
2020-05-01 15:38:02 -0400

    name changes

---
+++ Challenge_0.py
@@ -0,0 +1,8 @@
+# Challenge 1
+# Wrote by Rock_Z
+
+x = 8**16
+
+# print("http://tiaozhan.lanpujihua.com/", x, sep='')
+
+print("http://tiaozhan.lanpujihua.com/{}.html".format(x))
--- Challenge_1.py
+++ Challenge_1.py
@@ -1,8 +1,17 @@
-# Challenge 1
+# Challenge 2
 # Wrote by Rock_Z
 
-x = 8**16
+# Decoder below
 
-# print("http://tiaozhan.lanpujihua.com/", x, sep='')
+original_str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print("http://tiaozhan.lanpujihua.com/{}.html".format(x))
+decoded_str = ""
+
+for i in original_str:
+    num = ord(i)
+    if 122 >= num >= 97:
+        num = ((num+2)-97)%26+97
+    i = chr(num)
+    decoded_str = decoded_str + i
+
+print(decoded_str)
