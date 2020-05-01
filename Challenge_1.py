4793ca1f2acee983ff3819791aaf1e2c26427ba5
rockz <rockzhangenyan@gmail.com>
2020-05-01 15:54:40 -0400

    finished challenge 1 and corresponding python files

--- Challenge_1.py
+++ Challenge_1.py
@@ -3,7 +3,7 @@
 
 # Decoder below
 
-original_str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+original_str = ""
 
 decoded_str = ""
 
