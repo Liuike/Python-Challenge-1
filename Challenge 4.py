16460ccf9779215c8ec14f7201899ad573a209f2
rockz <rockzhangenyan@gmail.com>
2020-05-12 21:18:03 -0400

    added python file for challenge 4

---
+++ Challenge 4.py
@@ -0,0 +1,19 @@
+# 1到5000中有多少个质数
+
+import math
+
+Total_number = int(input('Please input number: '))
+Prime_number_count = 0
+Divisor = 0
+
+for i in range (Total_number):
+    num = i + 1
+    if num > 1:
+        Divisor = 0
+        for i in range (2, num):
+            if num % i == 0:
+                Divisor = Divisor + 1
+        if Divisor == 0:
+            Prime_number_count = Prime_number_count + 1
+
+print ("total prime number count =", str(Prime_number_count))
